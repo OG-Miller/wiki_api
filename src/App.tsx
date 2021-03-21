01047fa551c4ba8806297a392642d414f5b87f4c
Gavin Miller <gavinmiller12345@gmail.com>
2021-03-21 08:53:31 +0100

    border style/pre-grid push

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { ButtonHTMLAttributes, useEffect } from 'react';
 import { useState } from 'react';
 import logo from './logo.svg';
 import '././scss/App.scss';
@@ -17,7 +17,7 @@ interface pageDataObj {
 	extract: String | undefined;
 }
 
-// here we set the component as a React Functional Component
+// here we set the component to a React Functional Component
 const App: React.FC = () => {
 	const [infoFromSearch, setInfoFromSearch] = useState<dataObj[]>([]);
 	const [infoFromPageData, setInfoFromPageData] = useState<pageDataObj>();
@@ -27,9 +27,9 @@ const App: React.FC = () => {
 
 	// check if previous data is in local storage then set state to that data
 	useEffect(() => {
-		const data = localStorage.getItem('search-results');
-		if (data) {
-			setInfoFromSearch(JSON.parse(data));
+		const localSearchData = localStorage.getItem('search-results');
+		if (localSearchData) {
+			setInfoFromSearch(JSON.parse(localSearchData));
 		}
 	}, []);
 
@@ -38,22 +38,33 @@ const App: React.FC = () => {
 		localStorage.setItem('search-results', JSON.stringify(infoFromSearch));
 	}, [infoFromSearch]);
 
+	// NOTE for testing
 	useEffect(() => {
-		setShowArticle(true);
-	}, [infoFromPageData]);
+		console.log(showArticle);
+	}, [showArticle]);
 
 	const getSearchData = async () => {
-		const url = `http://en.wikipedia.org/w/api.php?prop=extracts&exintro&explaintext=true&redirects=1&action=query&list=search&srlimit=5&srsearch=${searchQuery}&utf8=&format=json&origin=*`;
-		const searchResponse = await fetch(url);
-		const searchData = await searchResponse.json();
-		setInfoFromSearch(searchData.query.search);
-		console.log(searchData);
+		if (searchQuery) {
+			const url = `http://en.wikipedia.org/w/api.php?prop=extracts&exintro&explaintext=true&redirects=1&action=query&list=search&srlimit=5&srsearch=${searchQuery}&utf8=&format=json&origin=*`;
+			const searchResponse = await fetch(url);
+			const searchData = await searchResponse.json();
+			setInfoFromSearch(searchData.query.search);
+			console.log(searchData);
+		}
+	};
+	// currently this sets everything to hidden, which isn't what we want
+	const handleHomeClick: (e: React.MouseEvent<HTMLButtonElement>) => void = e => {
+		setInfoFromSearch([]);
+		setInfoFromPageData(undefined);
+		// setShowArticle(false);
 	};
 
 	const handleSearch: (e: React.FormEvent<HTMLFormElement>) => void = e => {
 		e.preventDefault();
 		getSearchData();
 		setSearchQuery('');
+		setShowArticle(false);
+		setShowSearchResults(true);
 	};
 
 	const getPageData = async (id: number) => {
@@ -69,19 +80,26 @@ const App: React.FC = () => {
 		e.stopPropagation();
 		getPageData(id);
 		setShowSearchResults(false);
-		// setShowArticle(true);
+		setShowArticle(true);
 	};
 
 	return (
 		<div className='App'>
 			<div className='searchBar__wrapper'>
+				<button
+					onClick={e => {
+						handleHomeClick(e);
+					}}
+					className='home__button'>
+					home
+				</button>
 				<form className='searchBar' onSubmit={e => handleSearch(e)}>
 					<input type='text' value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
 					<button className='searchBar__button'>search</button>
 				</form>
 			</div>
 			<div className='data'>
-				{showSearchResults === true
+				{showSearchResults === true && infoFromSearch
 					? infoFromSearch.map(item => (
 							<div
 								key={item.pageid}
@@ -92,7 +110,7 @@ const App: React.FC = () => {
 							</div>
 					  ))
 					: null}
-				{infoFromPageData ? (
+				{infoFromPageData && showArticle ? (
 					<Article title={infoFromPageData.title} extract={infoFromPageData.extract} />
 				) : null}
 			</div>
